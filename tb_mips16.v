2a6e81725c31866c06681f14d728665736fddd4e
DhruvkBhatt <bdhruv32@gmail.com>
2020-03-09 23:04:30 +0530

    test_bench

---
+++ tb_mips16.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:07:11 03/09/2020
+// Design Name:   mips_16
+// Module Name:   E:/bits/ACA/New folder/mips_fpga4student/tb_mips16.v
+// Project Name:  mips_fpga4student
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mips_16
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+// Testbench Verilog code for 16 bit single cycle MIPS CPU  
+ module tb_mips16;  
+      // Inputs  
+      reg clk;  
+      reg reset;  
+      // Outputs  
+      wire [15:0] pc_out;  
+      wire [15:0] alu_result;//,reg3,reg4;  
+      // Instantiate the Unit Under Test (UUT)  
+      mips_16 uut (  
+           .clk(clk),   
+           .reset(reset),   
+           .pc_out(pc_out),   
+           .alu_result(alu_result)  
+           //.reg3(reg3),  
+          // .reg4(reg4)  
+      );  
+      initial begin  
+           clk = 0;  
+           forever #10 clk = ~clk;  
+      end  
+      initial begin  
+           // Initialize Inputs  
+           //$monitor ("register 3=%d, register 4=%d", reg3,reg4);  
+           reset = 1;  
+           // Wait 100 ns for global reset to finish  
+           #100;  
+     reset = 0;  
+	  #300;
+	  $finish;
+           // Add stimulus here  
+      end  
+ endmodule  
